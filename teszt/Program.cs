52ddce8f831d53b7fe9f380c4c407b829c25c3e7
2021143 <2021143@B109-07>
2024-09-11 11:51:11 +0200

    Add project files.

---
+++ teszt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teszt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello vilag!");
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
