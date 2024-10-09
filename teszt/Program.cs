545b0a68912ce808149b1295658305dd9cd9accc
Skristof1 <soreskristof18@gmail.com>
2024-10-09 11:57:31 +0200

    OK

--- teszt/Program.cs
+++ teszt/Program.cs
@@ -4,13 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace teszt
+namespace Korhenger
 {
-    class Program
+    class kor
     {
+        private double sugar, kerulet, terulet;
+
+        public kor() { }
+
+        public kor(double r)
+        {
+            this.sugar = r;
+            this.kerulet = kalkerulet(r);
+            this.terulet = kalterulet(r);
+        }
+
+        private double kalkerulet(double r)
+        {
+            return 2 * r * Math.PI
+        }
+
+        private double kalterulet(double r)
+        {
+            return Math.Pow(r, 2) * Math.PI;
+        }
+
+        public double GetSugar() { return this.sugar; }
+        public double GetKerulet() { return this.kerulet; }
+        public double GetTerulet() { return this.terulet; }
+
+        public void SetSugar(double r)
+
+        private void setDatas(double r)
+        {
+            this.sugar = r;
+            this.kerulet = kalkerulet(r);
+            this.terulet = kalterulet(r);
+        }
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello vilag!");
+
 
 
 
